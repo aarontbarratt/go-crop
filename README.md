224caf94a6233e832b1d04ffcace95ff133761d9
BanAaron <aarontbarratt@gmail.com>
2024-03-15 23:21:41 +0000

    getting args

---
+++ README.md
@@ -0,0 +1,20 @@
+# Go Crop
+
+Go crop is a CLI tool to quickly crop images.
+
+## How to Use
+
+Call go-crop with the `gocrop` command. Supply a `width`, `height`, `left`, and
+`top` position in pixels to control the dimensions of the resulting image.
+
+```shell
+gocrop source.jpg width height left top destination.jpg
+```
+
+## Setup
+
+```shell
+git clone git@github.com:BanAaron/go-crop.git
+cd go-crop
+go run main.go
+```
--- main.go
+++ main.go
@@ -1,61 +1,20 @@
 package main
 
 import (
-	"image"
-	"image/color"
-	"image/png"
-	"log"
-	"math/rand"
-	"os"
-	"time"
+	"flag"
+	"fmt"
+	"strings"
 )
 
-func main() {
-	const width int = 1919
-	const height int = 1080
-	var colours = [3]color.RGBA{Red, Green, Blue}
-
-	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-	randomImage := image.NewRGBA(image.Rect(0, 0, width, height))
-
-	for y := 0; y < randomImage.Bounds().Max.Y; y++ {
-		for x := 0; x < randomImage.Bounds().Max.X; x++ {
-			coloursLength := len(colours)
-			choice := random.Intn(coloursLength)
-			randomImage.SetRGBA(x, y, colours[choice])
-		}
-	}
-	out, err := os.Create("output.png")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	err = png.Encode(out, randomImage)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	err = out.Close()
-	if err != nil {
-		log.Fatalln(err)
-	}
+func showHelp() {
+	fmt.Println("To crop an image supply the following arguments:")
+	fmt.Println("gocrop source.jpg width height left top destination.jpg")
 }
 
-var (
-	Red = color.RGBA{
-		R: 255,
-		G: 0,
-		B: 0,
-		A: 255,
+func main() {
+	flag.Parse()
+
+	if strings.ToLower(flag.Arg(0)) == "help" || flag.NArg() == 0 {
+		showHelp()
 	}
-	Green = color.RGBA{
-		R: 0,
-		G: 255,
-		B: 0,
-		A: 255,
-	}
-	Blue = color.RGBA{
-		R: 0,
-		G: 0,
-		B: 255,
-		A: 255,
-	}
-)
+}
