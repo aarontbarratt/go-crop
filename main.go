c612ae8f79c67c26989bcb09db02772ade489f7b
BanAaron <aarontbarratt@gmail.com>
2024-03-16 23:55:08 +0000

    getting all arguments

--- main.go
+++ main.go
@@ -3,18 +3,41 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
+	"log"
+	"os"
+	"strconv"
 )
 
-func showHelp() {
-	fmt.Println("To crop an image supply the following arguments:")
-	fmt.Println("gocrop source.jpg width height left top destination.jpg")
+func handleError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
 func main() {
+	helpCommands := []string{"-h", "h", "-help", "help"}
+	for _, arg := range os.Args[1:] {
+		for _, command := range helpCommands {
+			if arg == command {
+				fmt.Println("example gocrop usage: gocrop <file> <width> <height> <left> <top>")
+				return
+			}
+		}
+	}
+
 	flag.Parse()
 
-	if strings.ToLower(flag.Arg(0)) == "help" || flag.NArg() == 0 {
-		showHelp()
+	if len(flag.Args()) != 5 {
+		log.Fatal("Invalid number of arguments")
 	}
+	file := flag.Arg(0)
+	width, err := strconv.Atoi(flag.Arg(1))
+	handleError(err)
+	height, err := strconv.Atoi(flag.Arg(2))
+	handleError(err)
+	left, err := strconv.Atoi(flag.Arg(3))
+	handleError(err)
+	right, err := strconv.Atoi(flag.Arg(4))
+	handleError(err)
+
+	fmt.Println(file, width, height, left, right)
 }
